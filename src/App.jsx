49e1197d4827389b5e365f6b71eb4c9ead017005
Gulshan Thakur <gulshanthakur205@gmail.com>
2023-11-03 21:25:29 +0530

    Added Sidebar ToggleButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,34 @@
-import "./app.scss"
+import "./app.scss";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
+import Home from "./components/homepage/Home";
 import Navbar from "./components/navbar/Navbar";
-
+import Project from "./components/projects/Project";
 
 const App = () => {
-  return <div>
-    <section id="Home">
-      <Navbar/>
-    </section>
-    
-    <section id="Skills">Skills</section>
-    <section id="About">About</section>
-    <section id="Projects">Project1</section>
-    <section>Project2</section>
-    <section>Project3</section>
-    <section id="Contact">Contact</section>
+  return (
+    <div>
+      <Cursor />
 
-  </div>
+      <section id="Homepage">
+        <Navbar />
+        <Home />
+      </section>
+
+      <section id="About">
+        <About type="about"/>
+        <Project/>
+      </section>
+
+      {/* Skills & project Des. */}
+      <Project/>
+
+      <section id="Contact">
+        <Contact />
+      </section>
+    </div>
+  );
 };
 
-
-export default App
+export default App;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,29 @@
-import Sidebar from '../sidebar/Sidebar'
-import './navbar.scss'
-import { motion } from 'framer-motion'
+import Sidebar from "../sidebar/Sidebar";
+import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        {/*Sidebar*/}
-        <Sidebar/>
-        <div className='wrapper'>
-            <motion.span 
-            initial={{opacity:0, scale:0.5}}
-            animate={{opacity:1, scale:1}}
-            transition={{duration:0.5}}>
-                Portfolio
-            </motion.span>
-            <div className='social'>
-                <a href='#'><img src='linkedin.png' alt=''/></a>
-                <a href='#'><img src='twitterx.png' alt=''/></a>
-                <a href='#'><img src='github.png' alt=''/></a>
-                <a href='#'><img src='instagram.png' alt=''/></a>
-            </div>
+    <div className="navbar">
+      {/* Sidebar */}
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h1>GT!!!</h1>
+        </motion.span>
+        <div className="social">
+          <a href='http://www.linkedin.com/in/gulshanthakur17'><img src='linkedin.png' alt='linkedin'/></a>
+          <a href='https://twitter.com/g_thakur17'><img src='twitterx.png' alt='twitter'/></a>
+          <a href='https://github.com/gulshanthakur17'><img src='github.png' alt='github'/></a>
+          <a href='https://www.instagram.com/mr__thakur17'><img src='instagram.png' alt='instagram'/></a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
